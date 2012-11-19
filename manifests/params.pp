566d3dce8f572765622d966d85827a7c59bf29ce
Ian Young <ian@iay.org.uk>
2012-11-19 21:55:58 +0000

    Support CentOS.  Explicitly fail for unknown operating systems.

--- manifests/params.pp
+++ manifests/params.pp
@@ -10,5 +10,20 @@ class denyhosts::params {
             $work_dir = '/var/lib/denyhosts'
             $lock_file = '/var/run/denyhosts.pid'
         }
+
+        centos: {
+            $package_name = 'denyhosts'
+            $service_name = 'denyhosts'
+            $service_pattern = 'python /usr/sbin/denyhosts'
+            $config_file = '/etc/denyhosts/denyhosts.cfg'
+            $secure_log = '/var/log/secure'
+            $hosts_deny = '/etc/hosts.deny'
+            $work_dir = '/usr/share/denyhosts/data'
+            $lock_file = '/var/lock/subsys/denyhosts'
+        }
+
+        default: {
+            fail("unknown OS $::operatingsystem")
+        }
     }
 }
